7f28dcd7f335d6bb98d950d312dc76ce4054f32e
Kevin Jacob Biju <kjb007kevin@gamil.com>
2019-02-23 14:50:44 +0530

    Flask App

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/home")
 def index():
-    return "hello"
-    return "Hello"
+    return "<h2>hello</h2><h3> hi</h3>"
 @app.route("/contact")
 def contact():
-    return "contact 787856851656"
+    return "contact  64668  5464687658 787856851656"
 @app.route("/error")
 def error():
-    return "GEt out"
+    return "Get out"
 if(__name__=="__main__"):
-    app.run()
+    app.run(debug=True)
 
